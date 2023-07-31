4324b0234eee7d17720b89367b7289ab366f11ef
Demyan <dyo171717@gmail.com>
2023-07-31 20:36:49 +0300

    doc: added description to ObjKeys and ObjEntries

--- src/public/object-entries.ts
+++ src/public/object-entries.ts
@@ -1,3 +1,21 @@
+/**
+ * @description type `ObjEntries<Obj>` intended for typing Object.entries() method
+ *
+ * @usage
+ *
+ * interface IUser {
+ *     name: string;
+ *     age: number;
+ *     address: {
+ *         country: string;
+ *         city: string;
+ *         ZIP: number;
+ *     };
+ * }
+ *
+ * `const entries = Object.entries() as ObjEntries<IUser>;`
+ */
+
 export type ObjEntries<Obj> = {
     [K in keyof Obj]: [K, Obj[K]];
 } [keyof Obj][];
--- src/public/object-keys.ts
+++ src/public/object-keys.ts
@@ -1,1 +1,19 @@
+/**
+ * @description type `ObjKeys<Obj>` intended for typing Object.keys() method
+ *
+ * @usage
+ *
+ * interface IUser {
+ *     name: string;
+ *     age: number;
+ *     address: {
+ *         country: string;
+ *         city: string;
+ *         ZIP: number;
+ *     };
+ * }
+ *
+ * `const keys = Object.keys() as ObjKeys<IUser>;` // keys - 'name' | 'age' | 'address'
+ */
+
 export type ObjKeys<Obj> = (keyof Obj)[];
